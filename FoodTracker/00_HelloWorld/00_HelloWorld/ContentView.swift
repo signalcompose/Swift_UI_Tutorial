d0429883c7b16f2e35a2e823d3dfe58677f1040b
leico <oishi@signalcompose.com>
2020-01-20 11:54:50 +0900

    first commit

---
+++ FoodTracker/00_HelloWorld/00_HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00_HelloWorld
+//
+//  Created by leico_studio on 2020/01/20.
+//  Copyright © 2020 leico_studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodTracker/00_HelloWorld/00_HelloWorldTests/_0_HelloWorldTests.swift
@@ -0,0 +1,34 @@
+//
+//  _0_HelloWorldTests.swift
+//  00_HelloWorldTests
+//
+//  Created by leico_studio on 2020/01/20.
+//  Copyright © 2020 leico_studio. All rights reserved.
+//
+
+import XCTest
+@testable import _0_HelloWorld
+
+class _0_HelloWorldTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
