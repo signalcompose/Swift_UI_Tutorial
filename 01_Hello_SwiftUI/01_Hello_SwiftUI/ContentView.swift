45c01cac8cc39058f67485c9e8d899eff696d364
leico <yoshitaka_12@iamas.ac.jp>
2020-02-03 18:16:01 +0900

    move upper directory and create new project 02_Hello_SwiftUI

---
+++ 01_Hello_SwiftUI/01_Hello_SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  01_Hello_SwiftUI
+//
+//  Created by leico_studio on 2020/02/03.
+//  Copyright © 2020 leico_studio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        //Text("Hello, SwiftUI!")
+          Text("Hello, SwiftUI!!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 01_Hello_SwiftUI/01_Hello_SwiftUITests/_1_Hello_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  _1_Hello_SwiftUITests.swift
+//  01_Hello_SwiftUITests
+//
+//  Created by leico_studio on 2020/02/03.
+//  Copyright © 2020 leico_studio. All rights reserved.
+//
+
+import XCTest
+@testable import _1_Hello_SwiftUI
+
+class _1_Hello_SwiftUITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
